7fe2bd1909a211799d761c489e4d138a071cb9af
Rajan Gupta <rajan0493@gmail.com>
2019-09-06 22:29:22 +0800

    Updated offers pubId

--- src/main/java/com/incentives/piggyback/offers/utils/constants/Constant.java
+++ src/main/java/com/incentives/piggyback/offers/utils/constants/Constant.java
@@ -2,7 +2,7 @@ package com.incentives.piggyback.offers.utils.constants;
 
 public interface Constant {
     String OFFER_PUBLISHER_TOPIC = "offersEvents";
-    String OFFER_SOURCE_ID = "3";
+    String OFFER_SOURCE_ID = "5";
 
     String OFFER_CREATED_EVENT = "Offers Events Created";
     String OFFER_UPDATED_EVENT = "Offers Events Updated";
