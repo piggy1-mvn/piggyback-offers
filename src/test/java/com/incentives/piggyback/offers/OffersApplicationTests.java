56c2347189b39b8a30e03f5c89de7ae1d196aa13
Rajan Gupta <rajan0493@gmail.com>
2019-09-11 16:56:25 +0800

    Migrate to powermock to improve coverage

--- src/test/java/com/incentives/piggyback/offers/OffersApplicationTests.java
+++ src/test/java/com/incentives/piggyback/offers/OffersApplicationTests.java
@@ -2,16 +2,25 @@ package com.incentives.piggyback.offers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
+@RunWith(PowerMockRunner.class)
 @SpringBootTest
 public class OffersApplicationTests {
 
 	@Test
+	@PrepareForTest(SpringApplication.class)
 	public void contextLoads() {
+		mockStatic(SpringApplication.class);
+		OffersApplication.main(new String[]{"Hello", "World"});
+		verifyStatic(SpringApplication.class);
+		SpringApplication.run(OffersApplication.class, "Hello", "World");
 	}
 
 }
